86a1316c12c5ee20d164cc2d9a17e4676d8e1f8f
Taejun Kim <taejun@kaist.ac.kr>
2023-06-09 16:41:53 +0900

    Fix bug

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
 import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/kit/vite'
+
+const dev = process.argv.includes('dev')
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
